bfaf340c9f708dde34f7bb410f366ca6371906b8
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2025-11-20 18:21:33 +0530

    feat: add logo and update links in header and footer

--- apps/web/src/app/components/footer.tsx
+++ apps/web/src/app/components/footer.tsx
@@ -1,20 +1,22 @@
-import { LinkedinIcon, TwitterIcon } from "lucide-react";
-
-import { Button } from "@repo/ui/components/button";
+import Image from "next/image";
 
 export default function Footer() {
   return (
     <footer>
       <div className="mx-auto max-w-7xl p-6 lg:px-8 flex flex-col justify-center items-center gap-3">
-        <div className="flex items-center justify-center gap-3">
-          <Button variant="outline" size="icon-lg" className="rounded-full">
-            <LinkedinIcon />
-          </Button>
-          <Button variant="outline" size="icon-lg" className="rounded-full">
-            <TwitterIcon />
-          </Button>
+        <div className="flex items-center justify-center text-7xl gap-10">
+          <Image
+            src="/plain-right-flower-logo.svg"
+            alt="One Game"
+            width={100}
+            height={100}
+          />
+          One Game
         </div>
-        <div>One Game.</div>
+
+        <p className="text-muted-foreground">
+          Copyright © <span>{new Date().getFullYear()}</span> One Game
+        </p>
       </div>
     </footer>
   );
--- apps/web/src/app/components/header.tsx
+++ apps/web/src/app/components/header.tsx
@@ -1,4 +1,7 @@
+import Image from "next/image";
+import Link from "next/link";
 import { PlayIcon, CircleQuestionMarkIcon } from "lucide-react";
+
 import { Button } from "@repo/ui/components/button";
 
 export default function Header() {
@@ -9,19 +12,29 @@ export default function Header() {
         className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
       >
         <div className="flex lg:flex-1">
-          <a href="/" className="-m-1.5 p-1.5">
-            <span className="sr-only">Your Company</span>
-            <div className="text-2xl">One Game.</div>
+          <a href="/" className="-m-1.5 p-1.5 flex items-center gap-2">
+            <Image
+              src="/plain-right-flower-logo.svg"
+              alt="One Game"
+              width={30}
+              height={30}
+            />
+            <span className="sr-only">One Game</span>
+            <div className="text-2xl">One Game</div>
           </a>
         </div>
 
         <div className="flex items-center justify-between gap-3">
           <Button variant="outline" size="icon-lg" className="rounded-full">
-            <CircleQuestionMarkIcon />
+            <Link href="/#how-to-play">
+              <CircleQuestionMarkIcon />
+            </Link>
           </Button>
 
           <Button variant="outline" size="icon-lg" className="rounded-full">
-            <PlayIcon />
+            <Link href="/play-the-board">
+              <PlayIcon />
+            </Link>
           </Button>
         </div>
       </nav>
