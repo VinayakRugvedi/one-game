e0994337fa8cbb751d59aaed8bc44d4f424a6ffe
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2025-11-21 18:29:50 +0530

    refactor: update components path, classnames, and meta description

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -2,14 +2,17 @@ import "@repo/ui/global.css";
 import type { Metadata } from "next";
 import { Space_Grotesk as SpaceGrotesk } from "next/font/google";
 
-import Header from "./components/header";
-import Footer from "./components/footer";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
 
-const spaceGrotesk = SpaceGrotesk({ subsets: ["latin"] });
+const spaceGrotesk = SpaceGrotesk({
+  subsets: ["latin"],
+  variable: "--font-space-grotesk",
+});
 
 export const metadata: Metadata = {
   title: "One Game",
-  description: "The internet's most minimalistic and premium board game.",
+  description: "The internet's most minimal and premium board game.",
   viewport: "width=device-width, initial-scale=1, maximum-scale=1",
 };
 
@@ -19,8 +22,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark" style={{ colorScheme: "dark" }}>
-      <body className={`${spaceGrotesk.className}`}>
+    <html
+      lang="en"
+      className="dark antialiased scroll-smooth"
+      style={{ colorScheme: "dark" }}
+    >
+      <body className={`${spaceGrotesk.className} font-space`}>
         <div className="bg-background text-foreground min-h-svh">
           <Header />
           <div className="min-h-screen">{children}</div>
